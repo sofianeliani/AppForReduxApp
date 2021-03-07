ea6ed1f75ec7c2e3a3a56ff49696e58ecde8d5aa
Sofiane LIANI <sofiane.liani@ecole-isitech.fr>
2021-03-07 18:17:52 +0100

    fix to api test 0.0.0.0

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "http://127.0.0.1:3000/api/";
+const API_URL = "http://0.0.0.0:3000/api/";
 
 const getPublicContent = () => {
   return axios.get(API_URL + "content");
