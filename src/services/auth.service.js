3815761215244b8522e3bbc38d7d8d3e09da235a
Sofiane LIANI <sofiane.liani@ecole-isitech.fr>
2021-03-07 18:15:01 +0100

    fix to api test 127.0.0.1

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/auth/";
+const API_URL = "http://127.0.0.1:3000/auth/";
 
 const register = (username, email, password) => {
   return axios.post(API_URL + "signup", {
