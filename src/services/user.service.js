b97364cd1a60292ec81d2480e9c3000f1e21d295
Sofiane LIANI <sofiane.liani@ecole-isitech.fr>
2021-03-07 17:13:22 +0100

    fix url to api 2

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "http://localhost:3000/api/";
+const API_URL = "http://18.185.46.151:3000/api/";
 
 const getPublicContent = () => {
   return axios.get(API_URL + "content");
