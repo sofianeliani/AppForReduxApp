3ceede91f3a30480cab7d362f98b11e57ea6a19e
Sofiane LIANI <sofiane.liani@ecole-isitech.fr>
2021-03-11 14:07:56 +0100

    fix url/ip for production

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "http://localhost:3000/api/";
+const API_URL = "/api/";
 
 const getPublicContent = () => {
   return axios.get(API_URL + "content");
