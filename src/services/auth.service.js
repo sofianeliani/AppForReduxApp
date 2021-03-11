5654faadd91c5bb27851b903144391f995d14648
Sofiane LIANI <sofiane.liani@ecole-isitech.fr>
2021-03-11 14:49:41 +0100

    fix url/ip for production

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "/auth/";
+const API_URL = "http://18.185.46.151/auth/";
 
 const register = (username, email, password) => {
   return axios.post(API_URL + "signup", {
