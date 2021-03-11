c9ff569ea038624d2487c73dfa5ba6dacccb883a
Sofiane LIANI <sofiane.liani@ecole-isitech.fr>
2021-03-11 21:38:38 +0100

    cors fix header

--- src/components/authComponent/login.component.js
+++ src/components/authComponent/login.component.js
@@ -45,36 +45,46 @@ const Login = (props) => {
   const handleLogin = (e) => {
     e.preventDefault();
 
-    let userCredentials = {
-      username: username,
-      password: password
-    }
+    // let userCredentials = {
+    //   username: username,
+    //   password: password
+    // }
 
     setLoading(true);
 
     form.current.validateAll();
 
     if (checkBtn.current.context._errors.length === 0) {
-
-      fetch('http://18.185.46.151:3000/api/auth/signin', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        credentials: 'include',
-        body: JSON.stringify(userCredentials)
+    dispatch(login(username, password))
+      .then(() => {
+        props.history.push("/profile");
+        window.location.reload();
       })
-        .then(response => response.json())
-        .then(data => localStorage.setItem('user', JSON.stringify(data)))
-        .catch(err => {
-          console.log(err)
-          setLoading(false);
-        })
-      }
-    else{
+      .catch(() => {
+        setLoading(false);
+      });
+    } else {
       setLoading(false);
     }
+  //     fetch('http://18.185.46.151:3000/api/auth/signin', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Accept': 'application/json',
+  //         'Content-Type': 'application/json'
+  //       },
+  //       credentials: 'include',
+  //       body: JSON.stringify(userCredentials)
+  //     })
+  //       .then(response => response.json())
+  //       .then(data => localStorage.setItem('user', JSON.stringify(data)))
+  //       .catch(err => {
+  //         console.log(err)
+  //         setLoading(false);
+  //       })
+  //     }
+  //   else{
+  //     setLoading(false);
+  //   }
   };
 
   if (isLoggedIn) {
