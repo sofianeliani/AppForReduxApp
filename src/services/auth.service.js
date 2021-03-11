b46e7009b75b18b0494f29d3b971643efebf6c4d
Sofiane LIANI <sofiane.liani@ecole-isitech.fr>
2021-03-11 22:00:01 +0100

    cors fix header

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -30,8 +30,7 @@ const login = (username, password) => {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
-    credentials: 'include',
-    body: JSON.stringify(userCredentials)
+    credentials: 'include'
     })
     .then(res => {
         if(res.data.accessToken) {
