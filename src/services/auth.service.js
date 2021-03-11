9aca5c7e7bba3bf90d2df12d8f0222c1918db76a
Sofiane LIANI <sofiane.liani@ecole-isitech.fr>
2021-03-11 17:17:52 +0100

    cors fix header

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -19,37 +19,37 @@ const register = (username, email, password) => {
 
 const login = (username, password) => {
 
-  return fetch(API_URL + 'signin', {
-    method: 'POST',
-    headers: headers,
-    body: {
-      username: username,
-      password: password
-    }
-  })
-  .then(response => {
-    response.json();
-  })
-  .then(data => {
-    if(data.accessToken){
-      localStorage.setItem("user", JSON.stringify(data.accessToken));
-    }
-  })
-  .catch(err => {
-    console.log(err)
-  })
-  // return axios
-  //   .post(API_URL + "signin", 
-  //   {
-  //     username,
-  //     password,
-  //   }, headers)
-  //   .then(res => {
-  //       if(res.data.accessToken) {
-  //           localStorage.setItem("user", JSON.stringify(res.data));
-  //       }
-  //       return res.data;
-  //   });
+  // return fetch(API_URL + 'signin', {
+  //   method: 'POST',
+  //   headers: headers,
+  //   body: {
+  //     username: username,
+  //     password: password
+  //   }
+  // })
+  // .then(response => {
+  //   response.json();
+  // })
+  // .then(data => {
+  //   if(data.accessToken){
+  //     localStorage.setItem("user", JSON.stringify(data.accessToken));
+  //   }
+  // })
+  // .catch(err => {
+  //   console.log(err)
+  // })
+  return axios
+    .post(API_URL + "signin", 
+    {
+      username,
+      password,
+    }, headers)
+    .then(res => {
+        if(res.data.accessToken) {
+            localStorage.setItem("user", JSON.stringify(res.data));
+        }
+        return res.data;
+    });
 };
 
 const logout = () => {
