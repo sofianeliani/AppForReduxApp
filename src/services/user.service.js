4dbbff592d540620259e1f070c63c14c6f5f04cb
Sofiane LIANI <sofiane.liani@ecole-isitech.fr>
2021-03-12 17:33:39 +0100

    cors fix header

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,23 +1,14 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "http://54.93.196.62/api/";
+const API_URL = "http://54.93.196.62:3000/api/";
 
 const getPublicContent = () => {
   return axios.get(API_URL + "content", { headers: authHeader() });
 };
 
 const getUserBoard = () => {
-  // return axios.get(API_URL + "usercontent", { headers: authHeader() });
-  // return fetch.get(API_URL + "usercontent", { headers: authHeader() });
-  fetch('http://54.93.196.62:3000/api/usercontent', {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      credentials: 'include'
-      })
+  return axios.get(API_URL + "usercontent", { headers: authHeader() });
 };
 
 const getAdminBoard = () => {
