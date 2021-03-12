93457853506128fadc19f581b62e53c942ffb34c
Sofiane LIANI <sofiane.liani@ecole-isitech.fr>
2021-03-12 12:07:03 +0100

    cors fix header

--- src/components/authComponent/login.component.js
+++ src/components/authComponent/login.component.js
@@ -64,7 +64,6 @@ const Login = (props) => {
     //   setLoading(false);
     // }
     fetch('http://18.157.163.253/api/auth/signin', {
-      mode: 'no-cors',
       method: 'POST',
       headers: {
         'Accept': 'application/json',
