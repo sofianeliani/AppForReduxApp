b7f39c7afe2b80c5284c2f1be6f329ca58078d7d
Sofiane LIANI <sofiane.liani@ecole-isitech.fr>
2021-03-12 16:56:28 +0100

    cors fix header

--- src/components/authComponent/login.component.js
+++ src/components/authComponent/login.component.js
@@ -52,36 +52,36 @@ const Login = (props) => {
     form.current.validateAll();
 
     if (checkBtn.current.context._errors.length === 0) {
-    //   dispatch(login(username, password))
-    //     .then(() => {
-    //       props.history.push("/profile");
-    //       window.location.reload();
-    //     })
-    //     .catch(() => {
-    //       setLoading(false);
-    //     });
-    // } else {
-    //   setLoading(false);
-    // }
-    fetch('http://54.93.196.62:3000/api/auth/signin', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      credentials: 'include',
-      body: JSON.stringify(userCredentials)
-      })
-      .then(response => response.json())
-      .then(data => localStorage.setItem('user', JSON.stringify(data)))
-      .catch(err => {
-        console.log(err)
-        setLoading(false);
-      })
-  } else {
-    setLoading(false);
-  }
-  };
+      dispatch(login(username, password))
+        .then(() => {
+          props.history.push("/profile");
+          window.location.reload();
+        })
+        .catch(() => {
+          setLoading(false);
+        });
+    } else {
+      setLoading(false);
+    }
+  //   fetch('http://54.93.196.62:3000/api/auth/signin', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Accept': 'application/json',
+  //       'Content-Type': 'application/json'
+  //     },
+  //     credentials: 'include',
+  //     body: JSON.stringify(userCredentials)
+  //     })
+  //     .then(response => response.json())
+  //     .then(data => localStorage.setItem('user', JSON.stringify(data)))
+  //     .catch(err => {
+  //       console.log(err)
+  //       setLoading(false);
+  //     })
+  // } else {
+  //   setLoading(false);
+  // }
+  // };
 
   if (isLoggedIn) {
     return <Redirect to="/profile" />;
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -16,17 +16,21 @@ const register = (username, email, password) => {
 };
 
 const login = (username, password) => {
-  return axios
-    .post(API_URL + "signin", {
-      username,
-      password,
-    }, {withCredentials: true})
-    .then(res => {
-        if(res.data.accessToken) {
-            localStorage.setItem("user", JSON.stringify(res.data));
-        }
-        return res.data;
-    });
+  fetch('http://54.93.196.62:3000/api/auth/signin', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    credentials: 'include',
+    body: JSON.stringify(userCredentials)
+    })
+    .then(response => response.json())
+    .then(data => localStorage.setItem('user', JSON.stringify(data)))
+    .catch(err => {
+      console.log(err)
+      setLoading(false);
+    })
 };
 
 const logout = () => {
