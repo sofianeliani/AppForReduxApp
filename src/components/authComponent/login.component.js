430e0c18e160d3c5fab354b9d48c3b7866e7beb4
Sofiane LIANI <sofiane.liani@ecole-isitech.fr>
2021-03-12 09:38:02 +0100

    cors fix header

--- src/components/authComponent/login.component.js
+++ src/components/authComponent/login.component.js
@@ -43,43 +43,44 @@ const Login = (props) => {
 
   const handleLogin = (e) => {
     e.preventDefault();
-    let userCredentials = {
-      username: username,
-      password: password
-    }
+    // let userCredentials = {
+    //   username: username,
+    //   password: password
+    // }
 
     setLoading(true);
 
     form.current.validateAll();
 
     if (checkBtn.current.context._errors.length === 0) {
-    // dispatch(login(username, password))
-    //   .then(() => {
-    //     props.history.push("/profile");
-    //     window.location.reload();
-    //   })
-    //   .catch(() => {
-    //     setLoading(false);
-    //   });
-    fetch('http://18.185.46.151:3000/api/auth/signin', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        credentials: 'include',
-        body: JSON.stringify(userCredentials)
-        })
-        .then(response => response.json())
-        .then(data => localStorage.setItem('user', JSON.stringify(data)))
-        .catch(err => {
-          console.log(err)
-          setLoading(false);
-        })
-    } else {
-      setLoading(false);
-    }
-  };
+    dispatch(login(username, password))
+      .then(() => {
+        props.history.push("/profile");
+        window.location.reload();
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+
+  //   fetch('http://18.185.46.151:3000/api/auth/signin', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Accept': 'application/json',
+  //         'Content-Type': 'application/json'
+  //       },
+  //       credentials: 'include',
+  //       body: JSON.stringify(userCredentials)
+  //       })
+  //       .then(response => response.json())
+  //       .then(data => localStorage.setItem('user', JSON.stringify(data)))
+  //       .catch(err => {
+  //         console.log(err)
+  //         setLoading(false);
+  //       })
+  //   } else {
+  //     setLoading(false);
+  //   }
+  // };
 
   if (isLoggedIn) {
     return <Redirect to="/profile" />;
